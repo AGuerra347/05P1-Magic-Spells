36fdfc4189aed66b929f704f4ac1769fcb339b95
AGuerra347 <43240350+AGuerra347@users.noreply.github.com>
2019-03-07 08:27:54 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#button").click(function() {
+    $(".black").fadeToggle();
+});
+
+$("#buttonTwo").click(function() {
+    $(".white").slideToggle();
+});
+
+$("#buttonThree").click(function() {
+    $(".purple").toggle();
+});
